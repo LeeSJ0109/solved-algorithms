bf0b6e96cdd7b764a1e13ec61a558641be54dac7
SJLEE <akacast01@gmail.com>
2024-07-02 23:08:30 +0900

    A+B

---
+++ Java/Baekjoon/수학/N1000.java
@@ -0,0 +1,14 @@
+package Java.Baekjoon.¼öÇÐ;
+
+import java.util.Scanner;
+
+public class N1000 {
+    public static void main(String[] args) {
+        int A, B;
+        Scanner input = new Scanner(System.in);
+        A = input.nextInt();
+        B = input.nextInt();
+
+        System.out.println(A + B);
+    }
+}
