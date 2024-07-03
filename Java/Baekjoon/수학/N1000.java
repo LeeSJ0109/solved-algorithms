35340713999835dace12d2be2320c70e9147606b
SJLEE <akacast01@gmail.com>
2024-07-04 08:20:17 +0900

    A+B

--- Java/Baekjoon/수학/N1000.java
+++ Java/Baekjoon/수학/N1000.java
@@ -1,11 +1,8 @@
-package Java.Baekjoon.Mathematics;
-
 import java.util.Scanner;
 
 public class N1000 {
     public static void main(String[] args) {
         int A, B;
-        @SuppressWarnings("resource")
         Scanner input = new Scanner(System.in);
         A = input.nextInt();
         B = input.nextInt();
