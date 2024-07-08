3c4e861677e254b3b4bb6f0b7d15bb75e8c74e6a
SJLEE <akacast01@gmail.com>
2024-07-08 22:38:58 +0900

    A+B

---
+++ Java/Baekjoon/수학/Baekjoon_1000.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Baekjoon_1000 {
+    public static void main(String[] args) {
+        int A, B;
+        Scanner sc = new Scanner(System.in);
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+        System.out.println(A + B);
+        sc.close();
+    }
+}
