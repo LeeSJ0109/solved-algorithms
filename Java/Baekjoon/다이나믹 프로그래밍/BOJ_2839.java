47711ac6079431f8b8eb4d889611ff3ab7154096
SJLEE <akacast01@gmail.com>
2024-07-20 22:29:28 +0900

    설탕 배달

--- Java/Baekjoon/다이나믹 프로그래밍/BOJ_2839.java
+++ Java/Baekjoon/다이나믹 프로그래밍/BOJ_2839.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Baekjoon_2839 {
+public class BOJ_2839 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
