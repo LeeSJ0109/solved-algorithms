629c505e6a45a20f577fdf71e86cb64cd6c974c9
SJLEE <akacast01@gmail.com>
2024-10-22 22:45:12 +0900

    최소비용 구하기

---
+++ Java/Baekjoon/최단 경로/BOJ_1916.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_1916 {
+
+    static class Node implements Comparable<Node> {
+        int v, cost;
+
+        Node(int v, int cost) {
+            this.v = v;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
+        }
+    }
+
+    static int N, M;
+    static List<Node>[] graph;
+
+    static int[] distance;
+    static boolean[] visited;
+
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        graph = new ArrayList[N + 1];
+        distance = new int[N + 1];
+        visited = new boolean[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        Arrays.fill(distance, Integer.MAX_VALUE);
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            graph[u].add(new Node(v, w));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        dijkstra(start);
+
+        System.out.println(distance[end]);
+    }
+
+    static void dijkstra(int start) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(start, 0));
+        distance[start] = 0;
+
+        while (!pq.isEmpty()) {
+            int cur = pq.poll().v;
+
+            if (visited[cur]) {
+                continue;
+            }
+            visited[cur] = true;
+
+            for (Node next : graph[cur]) {
+                if (distance[next.v] > distance[cur] + next.cost) {
+                    distance[next.v] = distance[cur] + next.cost;
+                    pq.add(new Node(next.v, distance[next.v]));
+                }
+            }
+        }
+    }
+}
