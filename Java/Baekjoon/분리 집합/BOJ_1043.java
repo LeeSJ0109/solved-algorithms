fd2847bba4ed7859acea6cf331475f55f8ad9317
SJLEE <akacast01@gmail.com>
2024-10-22 15:52:49 +0900

    거짓말

--- Java/Baekjoon/분리 집합/BOJ_1043.java
+++ Java/Baekjoon/분리 집합/BOJ_1043.java
@@ -6,6 +6,7 @@ public class BOJ_1043 {
     static int[] parent;
     static ArrayList<Integer>[] party;
 
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
