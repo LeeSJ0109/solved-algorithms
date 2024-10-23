ee29161463abe6fd72d669f2505807b38a7f2ff5
SJLEE <akacast01@gmail.com>
2024-10-24 08:15:40 +0900

    플로이드

---
+++ Java/Baekjoon/최단 경로/BOJ_11404.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_11404 {
+
+    static final int INF = 10_000_000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        int[][] cost = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    cost[i][j] = 0;
+                }
+                else {
+                    cost[i][j] = INF;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            cost[a][b] = Math.min(cost[a][b], c);
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    cost[i][j] = Math.min(cost[i][j], cost[i][k] + cost[k][j]);
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (cost[i][j] == INF) {
+                    System.out.print("0 ");
+                }
+                else {
+                    System.out.print(cost[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
