ff1312ffd021a80ee309daf16dc49ee8c25bdd10
SJLEE <akacast01@gmail.com>
2024-10-26 20:32:18 +0900

    벽 부수고 이동하기 2

---
+++ Java/Baekjoon/그래프 이론/BOJ_14442.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_14442 {
+	
+	static int N, M, K;
+	static char map[][];
+	static boolean visited[][][];
+	
+	static int[] dx = {-1, 1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		map = new char[N][M];
+		for (int i = 0; i < N; i++) {
+			map[i] = br.readLine().toCharArray();
+		}
+		
+		visited = new boolean[N][M][K + 1];
+		
+		System.out.println(BFS());
+	}
+	
+	static int BFS() {
+		Queue<int[]> queue = new LinkedList<>();
+		queue.add(new int[] {0, 0, K});
+		visited[0][0][K] = true;
+		
+		int moveCnt = 1;
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			
+			for (int i = 0; i < size; i++) {
+				int[] cur = queue.poll();
+				int x = cur[0];
+				int y = cur[1];
+				int cnt = cur[2];
+				
+				if (x == N - 1 && y == M - 1) {
+					return moveCnt;
+				}
+				
+				for (int d = 0; d < 4; d++) {
+					int nx = x + dx[d];
+					int ny = y + dy[d];
+					
+					if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
+						continue;
+					}
+					
+					if (map[nx][ny] == '1') {
+						if (cnt > 0 && !visited[nx][ny][cnt - 1]) {
+							queue.add(new int[] {nx, ny, cnt - 1});
+							visited[nx][ny][cnt - 1] = true;
+						}
+					} 
+					else if (!visited[nx][ny][cnt]){
+						queue.add(new int[] {nx, ny, cnt});
+						visited[nx][ny][cnt] = true;
+					}
+				}
+			}
+			
+			moveCnt++;
+		}
+		
+		return -1;
+	}
+}
