3df23d686199f05a9da323fdf3e636415796c5c1
이성준 <akacast01@gmail.com>
2025-03-27 21:58:52 +0900

    특정한 최단 경로

--- Java/Baekjoon/최단 경로/BOJ_1504.java
+++ Java/Baekjoon/최단 경로/BOJ_1504.java
@@ -22,6 +22,7 @@ public class BOJ_1504 {
     static ArrayList<Edge>[] graph;
     static int[] distance;
 
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
