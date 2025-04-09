dfe835a423beb1474e4f14522d6153477ba789d5
이성준 <akacast01@gmail.com>
2025-04-09 22:12:58 +0900

    정보 상인 호석

--- Java/Baekjoon/자료 구조/BOJ_22252.java
+++ Java/Baekjoon/자료 구조/BOJ_22252.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class BOJ_22252 {
-    @SuppressWarnings("rawtypes")
+    @SuppressWarnings({ "rawtypes", "unchecked" })
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
