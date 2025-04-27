0f1567c20afc55e4220d6eb01f338788ae6f8945
이성준 <akacast01@gmail.com>
2025-04-27 21:45:03 +0900

    줄 세우기

---
+++ Java/Baekjoon/위상 정렬/BOJ_2252.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ_2252 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] indegree = new int[N + 1];
+
+        List<Integer>[] graph = new List[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            graph[A].add(B);
+            indegree[B]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            if (indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            sb.append(current).append(" ");
+
+            for (int next : graph[current]) {
+                indegree[next]--;
+                if (indegree[next] == 0) {
+                    queue.add(next);
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
