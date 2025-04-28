ff9dc51ff584a852272d391a339fc7a7a9530b7d
이성준 <akacast01@gmail.com>
2025-04-28 21:45:17 +0900

    줄 세우기

--- Java/Baekjoon/위상 정렬/BOJ_2252.java
+++ Java/Baekjoon/위상 정렬/BOJ_2252.java
@@ -3,6 +3,7 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class BOJ_2252 {
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
