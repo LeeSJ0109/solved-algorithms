e9b3c227c7c703a0f12e370b0cc1a0c9a232b4c9
이성준 <akacast01@gmail.com>
2025-08-12 23:38:25 +0900

    조이스틱

---
+++ programmers/Lv.2/42860. 조이스틱/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(String name) {
+        int answer = 0;
+        int len = name.length();
+        
+        for (int i = 0; i < len; i++) {
+            answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
+        }
+        
+        int move = len - 1;
+        for (int i = 0; i < len; i++) {
+            int next = i + 1;
+            
+            while (next < len && name.charAt(next) == 'A') {
+                next++;
+            }
+            
+            move = Math.min(move, i * 2 + (len - next));
+            move = Math.min(move, (len - next) * 2 + i);
+        }
+        
+        return answer + move;
+    }
+}
