afad7c7481b725a038a032290b08f89889f6f17d
이성준 <akacast01@gmail.com>
2025-11-25 16:34:52 +0900

    부분합

--- baekjoon/Gold/1806. 부분합/BOJ_1806.java
+++ baekjoon/Gold/1806. 부분합/BOJ_1806.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class BOJ_1806 {
     public static void main(String[] args) throws IOException {
@@ -10,30 +8,30 @@ public class BOJ_1806 {
         int N = Integer.parseInt(st.nextToken());
         int S = Integer.parseInt(st.nextToken());
 
-        int[] sequence = new int[N];
-
+        int[] seq = new int[N];
+        
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            sequence[i] = Integer.parseInt(st.nextToken());
+            seq[i] = Integer.parseInt(st.nextToken());
         }
 
         int left = 0;
         int right = 0;
 
         int sum = 0;
-        int minCount = Integer.MAX_VALUE;
+        int minLength = Integer.MAX_VALUE;
 
         while (right < N) {
-            sum += sequence[right];
+            sum += seq[right];
             right++;
 
             while (sum >= S) {
-                minCount = Math.min(minCount, right - left);
-                sum -= sequence[left];
+                minLength = Math.min(minLength, right - left);
+                sum -= seq[left];
                 left++;
             }
         }
 
-        System.out.println(minCount == Integer.MAX_VALUE ? 0 : minCount);
+        System.out.println(minLength == Integer.MAX_VALUE ? 0 : minLength);
     }
 }
